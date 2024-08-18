b8f455dc25b5f2dc902c7a122adf3bf93a5b9075
msawired <sinan@openprocessing.org>
2024-08-18 23:32:33 +0200

    init: test worked with TouchOSC!

--- opc.js
+++ opc.js
@@ -3,7 +3,91 @@ class OPC {
 	constructor() {
 		this.options = {};
 		this.collapsed = false;
+		this.osc = null;
 	}
+
+	static setOSC(server, port = null) {
+		// you can set the OSC server and port here
+		// this will be used to send OSC messages when a variable changes
+		// This will also be used to receive OSC messages to update the variables
+
+		//load the OSC library
+		if (typeof window.OSC === 'undefined') {
+			let script = document.createElement('script');
+			script.onload = function () {
+				console.log('OSC library loaded');
+				//initialize OSC
+				OPC.loadOSC(server, port);
+			};
+			script.onerror = function () {
+				console.log('OSC library failed to load');
+			};
+			script.src = 'https://cdn.jsdelivr.net/npm/osc@2.4/dist/osc-browser.min.js';
+			document.head.appendChild(script);
+		}
+	}
+
+	static loadOSC(server, port = null) {
+		//initialize OSC
+		OPC.osc = new osc.WebSocketPort({
+			url: `${server}` + (port ? `:${port}` : ''), // URL to your Web Socket server.
+			metadata: true
+		});
+		OPC.osc.on('open', function () {
+			console.log('OSC connection opened');
+		});
+		OPC.osc.on('close', function () {
+			console.log('OSC connection closed');
+		});
+		OPC.osc.on('error', function (error) {
+			console.log('OSC error:', error);
+		});
+		OPC.osc.on('message', function (message) {
+			//set variables based on message
+			// console.log('OSC message:', message);
+			if (message.address) {
+				let variableName = message.address.slice(1); //remove the first slash
+				if (OPC.options[variableName]) {
+					if (message.args.length === 1) {
+						let value = message.args[0].value;
+						if (message.args[0].type === 'f') {
+							value = parseFloat(value);
+						} else if (message.args[0].type === 'i') {
+							value = parseInt(value);
+						} else if (message.args[0].type === 's') {
+							value = value.split(',');
+						}
+						OPC._set(variableName, value);
+					}
+				}
+			}
+		});
+		OPC.osc.on('ready', function () {
+			console.log('OSC ready');
+		});
+		OPC.osc.open();
+	}
+	static oscSendMessage(varName, value) {
+		let type;
+		if (Array.isArray(value)) {
+			type = 's';
+			value = value.join(',');
+		} else if (typeof value === 'string') {
+			type = 's';
+		} else if (typeof value === 'number') {
+			type = 'f';
+		} else if (typeof value === 'boolean') {
+			type = 'i';
+			value = value ? 1 : 0;
+		}
+		OPC.osc.send({
+			address: `/${varName}`,
+			args: [
+				{ type, value }
+			]
+		});
+	}
+
 
 	static slider(variableNameOrConfig, value, min = 0, max = null, step = null) {
 		let variableName, label, description;
@@ -201,10 +285,9 @@ class OPC {
 				return OPC.options[option.name].value;
 			},
 			set: function (value) {
-				OPC.options[option.name].value = value;
-				OPC.callParentFunction('OPC', OPC.options[option.name]);
-				if (typeof window.parameterChanged == 'function') {
-					window.parameterChanged(option.name, value);
+				OPC._set(option.name, value);
+				if (OPC.osc) {
+					OPC.oscSendMessage(option.name, value);
 				}
 			}
 		});
@@ -212,18 +295,22 @@ class OPC {
 		return true;
 	}
 
-	static set (variableName, value) {
-		window[variableName] = value;
+	static _set(variableName, value) {
+		OPC.options[variableName].value = value;
+		OPC.callParentFunction('OPC', OPC.options[variableName]);
+		if (typeof window.parameterChanged == 'function') {
+			window.parameterChanged(variableName, value);
+		}
 	}
 
-	static buttonPressed (variableName, value) {
+	static buttonPressed(variableName, value) {
 		OPC.options[variableName].value = value;
 		if (typeof window.buttonPressed == 'function') {
 			window.buttonPressed(variableName, value);
 		}
 
 	}
-	static buttonReleased (variableName, value) {
+	static buttonReleased(variableName, value) {
 		OPC.options[variableName].value = value;
 		if (typeof window.buttonReleased == 'function') {
 			window.buttonReleased(variableName, value);
@@ -231,23 +318,23 @@ class OPC {
 
 	}
 
-	static collapse () {
+	static collapse() {
 		OPC.collapsed = true;
 		OPC.callParentFunction('OPC_collapsed', OPC.collapsed);
 	}
-	static expand () {
+	static expand() {
 		OPC.collapsed = false;
 		OPC.callParentFunction('OPC_collapsed', OPC.collapsed);
 	}
-	static delete (variableName) {
-		if (OPC.options[variableName]){
+	static delete(variableName) {
+		if (OPC.options[variableName]) {
 			delete OPC.options[variableName];
 			delete window.variableName;
 		}
 		OPC.callParentFunction('OPC_delete', variableName);
 	}
 
-	static callParentFunction (functionName, arg = {}) {
+	static callParentFunction(functionName, arg = {}) {
 		// console.log(arg);
 		try {
 			//try sending as is
